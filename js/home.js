2d68517599211f618851bbe18f5085927eb53593
Taha Hachana <tahahachana@gmail.com>
2014-10-16 10:01:27 +0100

    Supporters and testimonials shuffling

--- js/home.js
+++ js/home.js
@@ -29,6 +29,25 @@ function feedLoaded(result) {
     }
 }
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 $(function () {
     $.ajax({
         url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=http%3A%2F%2Ffpish.net%2Frss%2Fblogs%2Ftag%2F1%2Ff~23',
@@ -36,5 +55,51 @@ $(function () {
         success: function (data) {
             feedLoaded(data.responseData.feed);
         }
+    });
+
+    // Corporate supporters shuffling
+    var carouselLink = $(".carousel-link");
+    var shuffled = shuffle(carouselLink.toArray());
+    var hrefs = $.map(shuffled, function (x) {
+        return x.getAttribute("href");
+    });
+    var srcs = $.map(shuffled, function (x) {
+        return $(".carousel-img", x).attr("src");
+    });
+    carouselLink.each(function (index, x) {
+        x.setAttribute("href", hrefs[index]);
+        $(".carousel-img", x).attr("src", srcs[index]);
+    });
+
+    // Testimonials shuffling
+    $.getJSON("testimonials.json", function (data) {
+        var arr = shuffle(data).slice(0, 6);
+        var testimonials = $.map(arr, function (x) {
+            var b = document.createElement("blockquote");
+            var p = document.createElement("p");
+            p.textContent = x.Text;
+            var f = document.createElement("footer");
+            var c = document.createElement("cite");
+            var a = document.createElement("a");
+            a.setAttribute("href", x.Link);
+            a.setAttribute("target", "_blank");
+            a.textContent = x.Author;
+            c.appendChild(a);
+            f.appendChild(c);
+            b.appendChild(p);
+            b.appendChild(f);
+            return b;
+        });
+
+        var col1 = $("#testimonials-col-1");
+        var col2 = $("#testimonials-col-2");
+        col1.empty();
+        col2.empty();
+        $.each(testimonials.slice(0, 3), function (i, x) {
+            col1.append(x);
+        });
+        $.each(testimonials.slice(3), function (i, x) {
+            col2.append(x);
+        });
     });
 });
